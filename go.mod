700dcc1b9ed454ec8f075c38f939973b19a3b0e9
srini <srinivasareddy.kamatham@quest.com>
2024-06-24 06:29:21 +0530

    Inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srinucdac/my-go-app
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,16 @@
+// main.go
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+    http.ListenAndServe(":8080", nil)
+}
